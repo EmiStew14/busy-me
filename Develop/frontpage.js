35c760c2e620731bf6c79ba27d3b204d57d5e631
Emily Stewart <stewartemilym2014@gmail.com>
2021-03-04 20:34:48 -0700

    added time divs and textarea to html

---
+++ Develop/frontpage.js
@@ -0,0 +1,14 @@
+//document.ready, jQuery method, wrap everything in this so it only loads java when DOM is ready.
+$(document).ready(function (){
+    //updater function (theres only 9 slots so it shouldn't be too painful worry about update update)
+        //time checker,time formater or some function for the date using moment.js function to html or hardcore it.
+    //click event for savebutton
+
+    //textarea in html = click event for text DONT WORRY
+
+    //time checker,time formater or some function for the date using moment.js function to html or hardcore it. look closely there is a div already created for you can also look 
+    //at jquery for ways to populate.
+
+    //main key is to select items
+    $(this).//figure out which ids and classes to grab
+})
