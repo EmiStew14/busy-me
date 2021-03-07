9871c83f5df8d45a175d4fcc5cd722c815b99aae
Emily Stewart <stewartemilym2014@gmail.com>
2021-03-07 13:56:32 -0700

    corrected time syntax, and loading storage functions

--- Develop/frontpage.js
+++ Develop/frontpage.js
@@ -1,46 +1,118 @@
 //document.ready, jQuery method, wrap everything in this so it only loads java when DOM is ready.
-var tasks = document.getElementById("toDo");
+// var save = document.getElementsByClassName("saveBtn");
 var current = moment().format('dddd MMMM Do, YYYY');
-var present = moment().format('h');
-console.log(present);
-var past = moment ().format('h');
-console.log(past);
-var future = moment ().format('h');
-console.log(future);
+var time = moment().format('H');
+// var selectTime = document.getElementsByClassName("hour").innerHTML;
+var tasks = document.getElementsByClassName("time-block");
 
 $("span").append(current);
 
 //   $(document).ready(function (){
     function hours() {
-     moment().calendar({
-      sameDay: function (now) {
-        if (this.isAfter(now)) {
-         past === $("toDo").addClass("past");
-          return ;
-      };
-      if (this.isBefore(now)) {
-          future === $("toDo").addClass("future");
-          return
-      };
-      if (this.isNow()) {
-          present === $("toDo").addClass("present");
-      };
-       }
-    });
-};
-    function saveMe() {
-        var userInput = document.getElementById("toDo").nodeValue;
-        localStorage.setItem("userInput",userInput);
-        console.log(userInput);
+    //  moment().calendar({
+    //   sameDay: function (now) {
+        // currentTime = parseInt(selectTime);
+        // console.log(selectTime);
+
+        $(".hour").each(function(){
+            var isPm = $(this).text().includes("p");
+            console.log(isPm);
+            var selectTime = parseInt($(this).text().split(" ")[0]);
+            if (isPm && selectTime!==12) {
+                selectTime+=12;
+            }
+            console.log(selectTime, time);
+             if (selectTime < time) {
+                 $(this).siblings("textarea").addClass("past");
+            };
+             if (selectTime > time) {
+                 $(this).siblings("textarea").addClass("future");
+             };
+             if (selectTime == time) {
+                $(this).siblings("textarea").addClass("present");
+             };
+        });
+    //     for (let i = 9; i < 18; i++) {
+    //     let currentTime = $(`#${i}`).attr("id");
+    //     console.log(currentTime);
+        
+    //     if (this.isAfter(now)) {
+    //      past < now;
+    //      $(`#${i}`).addClass("past");
+    //       return ;
+    //   } if (this.isBefore(now)) {
+    //       future > now;
+    //       $(`#${i}`).addClass("future");
+    //       return
+    //   }if (this.isNow()) {
+    //       present == now;
+    //       $(`#${i}`).addClass("present");
+    //        return
+    //   };
+    //    }
     };
+    hours();
+//     });
+// };
+// for (let i = 9; i < 17; i++) {
+//     let selectTime = $(`#${i}`).attr("id");
+//     console.log(selectTime);
+// };
+
+// function saveMe() {
+//     var userInput = document.getElementById("toDo").value;
+//     // userInput.push();
+//     localStorage.setItem(selectTime,userInput);
+//     console.log(userInput);
+// };
+function loadIt() {
+
+    // if (!tasks) {
+    //     tasks = {
+    //       toDo: [],
+    //       inProgress: [],
+    //       inReview: [],
+    //       done: []
+    //     };
+    //   }
+     var output = localStorage.getItem(selectTime, userInput);
+     var values = [],
+        keys = Object.keys(localStorage),
+         i = keys.length;
     
-    function loadIt() {
-        var userInput = localStorage.getItem("userInput");
-        document.getElementById("toDo").value = userInput;
-        console.log(userInput);
-    };
-    $(".saveBtn").click(saveMe());
-    console.log("bacon");
+     while ( i-- ) {
+        values.push( localStorage.getItem(keys[i],("textarea").val) );
+        }
+    $("textarea").append(values);
+     return values;
+};
+
+$(".saveBtn").on("click", function(){
+    const schedAct = $(this).siblings("textarea").val();
+    const btnID = $(this).attr("id");
+    localStorage.setItem(btnID, schedAct);
+  })
+  $("#9-toDo").val(localStorage.getItem("9"));
+  $("#10-toDo").val(localStorage.getItem("10"));
+  $("#11-toDo").val(localStorage.getItem("11"));
+  $("#12-toDo").val(localStorage.getItem("12"));
+  $("#1-toDo").val(localStorage.getItem("1"));
+  $("#2-toDo").val(localStorage.getItem("2"));
+  $("#3-toDo").val(localStorage.getItem("3"));
+  $("#4-toDo").val(localStorage.getItem("4"));
+  $("#5-toDo").val(localStorage.getItem("5"));
+//     $(document).ready(function (){
+//     $(".saveBtn").on("click",(){
+//         var butn = $(this).attr("id");
+//         // $(".hour").val(butn);
+//         console.log(butn);
+//         saveMe();
+//         console.log(saveMe);
+//         loadIt();
+//         console.log(loadIt);
+//     });
+// });
+
 //     //updater function (theres only 9 slots so it shouldn't be too painful worry about update update)
 //         //time checker,time formater or some function for the date using moment.js function to html or hardcore it.
 //     //click event for savebutton
